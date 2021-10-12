cc71fe52a61f7966a69b1a2d4f82b7f7190965bc
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-10-12 21:14:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.selector = selector;
+    // this.selector = selector;
     this.targetDate = targetDate;
   }
+
+  intervalId = setInterval(() => {
+    const time = countDownTimer.targetDate - new Date();
+
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    document
+      // .querySelector(countDownTimer.selector)
+      .querySelector('[data-value="days"]').textContent = days;
+    document
+      // .querySelector(countDownTimer.selector)
+      .querySelector('[data-value="hours"]').textContent = hours;
+    document
+      // .querySelector(countDownTimer.selector)
+      .querySelector('[data-value="mins"]').textContent = mins;
+    document
+      // .querySelector(countDownTimer.selector)
+      .querySelector('[data-value="secs"]').textContent = secs;
+
+    if (time < 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
 const countDownTimer = new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("<Mar 11, 2022"),
+  selector: '#timer-1',
+  targetDate: new Date('<Mar 11, 2022'),
 });
-
-const timerSelector = document.querySelector(countDownTimer.selector);
-
-const intervalId = setInterval(() => {
-  const time = countDownTimer.targetDate - new Date();
-
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-  timerSelector.querySelector('[data-value="days"]').textContent = days;
-  timerSelector.querySelector('[data-value="hours"]').textContent = hours;
-  timerSelector.querySelector('[data-value="mins"]').textContent = mins;
-  timerSelector.querySelector('[data-value="secs"]').textContent = secs;
-
-  if (time < 0) {
-    clearInterval(intervalId);
-  }
-}, 1000);
